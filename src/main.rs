b748c0e74d406c5806c0712be08fb1cd481b3af7
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2020-04-17 00:49:17 +0100

    Added function to read file to string, basic iteration over lex string

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,29 @@
-#[allow(dead_code)]
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+
+use std::fs;
+use std::io;
 
 mod token;
 
 fn main() {
-    println!("Hello, world!");
+    let file: String = match read_markdown_file(String::from("test/heading.md")) {
+        Ok(v) => v,
+        Err(e) => panic!("{}", e),
+    };
+
+    lex(file)
+}
+
+fn read_markdown_file(file_path: String) -> Result<String, io::Error> {
+    let file: String = fs::read_to_string(file_path)?;
+
+    Ok(file)
+}
+
+fn lex(text: String) {
+    for c in text.chars().enumerate().peekable() {
+        println!("char: {:?}", c);
+    }
 }
