87d496cf03982772a9e12c388d95856fb62841a9
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2020-04-21 00:38:39 +0100

    Updated lib.rs and moved tests from test.rs...
    This make more sense and other tests without logging will later be added to test.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,97 +3,23 @@
 use std::fs;
 use std::io;
 use std::io::Write;
-use std::iter;
-use std::str;
-use std::option;
 
 mod position;
 mod token;
 mod emphasis_state;
-mod match_markdown;
+mod markdown;
+mod lexer;
 
 pub use token::Token;
 pub use token::TokenType;
 
-use position::Position;
-use emphasis_state::Tag;
-use emphasis_state::State;
-
 pub fn markdown_to_html(input: &str, output: &str) -> Result<Vec<Token>, io::Error> {
     let text: String = fs::read_to_string(input)?;
-    let tokens = lex(&text);
+    let tokens = lexer::lex(&text);
     let html = parse(&text, &tokens);
     generate_html(output.to_string(), html);
 
     Ok(tokens)
-}
-
-fn lex(text: &String) -> Vec<Token> {
-    let mut tokens: Vec<Token> = Vec::with_capacity(text.len());
-    let mut iter = text.chars().enumerate().peekable();
-    let mut pos: Position = Position::new(0, 0, 0);
-    let mut state: State = State::new();
-    loop {
-        match iter.next() {
-            Some(c) => {
-                pos.increment();
-                match c.1 {
-                    '#' => {
-                        match_markdown::match_heading(&mut tokens, &mut iter, &mut pos, c);
-                    },
-                    '-' => {
-                        match iter.peek() {
-                            Some(v) => {
-                                match v.1 {
-                                    '-' => match_markdown::match_horizontalrule(text, &mut tokens, &mut iter, &mut pos, c),
-                                    ' ' => match_markdown::match_checkbutton(text, &mut tokens, &mut iter, &mut pos, c),
-                                    _ => tokens.push(Token::new_single(TokenType::Text, c.0)),
-                                }
-                            },
-                            None => tokens.push(Token::new_single(TokenType::Text, c.0)),
-                        }
-                    },
-                    '!' => {
-                        match_markdown::match_image(text, &mut tokens, &mut iter, &mut pos, c);
-                    },
-                    '[' => {
-                        match_markdown::match_link(text, &mut tokens, &mut iter, &mut pos, c);
-                    },
-                    '>' => {
-                        match_markdown::match_blockquote(text, &mut tokens, &mut iter, &mut pos, c);
-                    },
-                    '`' => {
-                        match iter.peek() {
-                            Some(v) => {
-                                match v.1 {
-                                    '`' => match_markdown::match_codeblock(text, &mut tokens, &mut iter, &mut pos, c),
-                                    _ => match_markdown::match_code(text, &mut tokens, &mut iter, &mut pos, c),
-                                }
-                            },
-                            None => tokens.push(Token::new_single(TokenType::Text, c.0)),
-                        }
-                    },
-                    ' ' => match iter.peek() {
-                        Some(v) => {
-                            match v.1 {
-                                ' ' => match_markdown::match_indentblock(text, &mut tokens, &mut iter, &mut pos, c),
-                                _ => tokens.push(Token::new_single(TokenType::Space, c.0)),
-                            }
-                        },
-                        None => tokens.push(Token::new_single(TokenType::Space, c.0)),
-                    },
-                    '*'|'~'|'_' => match_markdown::match_emphasis(&mut state, text, &mut tokens, &mut iter, &mut pos, c),
-                    '\n' => tokens.push(Token::new_single(TokenType::Newline, c.0)),
-                    '\t' => tokens.push(Token::new_single(TokenType::Tab, c.0)),
-                    '\\' => tokens.push(Token::new_single(TokenType::Escape, c.0)),
-                    _ => tokens.push(Token::new_single(TokenType::Text, c.0)),
-                }
-            },
-            None => break,
-        }
-    }
-
-    tokens
 }
 
 fn parse(text: &String, tokens: &Vec<Token>) -> Vec<String> {
@@ -243,269 +169,38 @@ mod tests {
     use super::*;
 
     #[test]
-    fn heading() -> Result<(), io::Error> {
-        let t = lex(&fs::read_to_string("tests/heading.md")?);
-        let mut headings: usize = 0;
-        let mut errors: usize = 0;
-        for token in t.iter() {
-            match token.id {
-                TokenType::Heading => {
-                    headings += 1;
-                },
-                TokenType::Error => {
-                    errors += 1;
-                },
-                TokenType::Text|TokenType::Space|TokenType::Newline|TokenType::Whitespace(' ') => (),
-                _ => panic!("Encounterd TokenType other than expected!"),
+    fn log() -> Result<(), io::Error> {
+        let test_files = [
+            "heading",
+            "checkbutton",
+            "image",
+            "link",
+            "horizontalrule",
+            "blockquote",
+            "code",
+            "codeblock",
+            "indentblock",
+            "escape",
+            "emphasis",
+            "html",
+        ];
+
+        for test in test_files.iter() {
+            let r = markdown_to_html(format!("tests/{}.md", test).as_str(),
+                                    format!("generated_html/{}.html", test).as_str());
+
+            match r {
+                Ok(tokens) => log_tokens(tokens, test)?,
+                Err(e) => return Err(e),
             }
         }
-        assert!(headings == 6);
-        assert!(errors == 1);
 
         Ok(())
     }
 
-    #[test]
-    fn checkbutton() -> Result<(), io::Error> {
-        let t = lex(&fs::read_to_string("tests/checkbutton.md")?);
-        let mut checkbuttons: usize = 0;
-        for token in t.iter() {
-            match token.id {
-                TokenType::Checkbutton(bool) => {
-                    checkbuttons += 1;
-                },
-                TokenType::Text|TokenType::Space|TokenType::Newline|TokenType::Whitespace(' ') => (),
-                _ => panic!("Encounterd TokenType other than expected!"),
-            }
-        }
-        assert!(checkbuttons == 2);
-
-        Ok(())
-    }
-
-    #[test]
-    fn image() -> Result<(), io::Error> {
-        let t = lex(&fs::read_to_string("tests/image.md")?);
-        let mut image_alt: usize = 0;
-        let mut image_src: usize = 0;
-        let mut errors: usize = 0;
-        for token in t.iter() {
-            match token.id {
-                TokenType::ImageAlt => {
-                    image_alt += 1;
-                },
-                TokenType::ImageSrc => {
-                    image_src += 1;
-                },
-                TokenType::Error => {
-                    errors += 1;
-                },
-                TokenType::Text|TokenType::Space|TokenType::Newline|TokenType::Whitespace(' ') => (),
-                _ => panic!("Encounterd TokenType other than expected!"),
-            }
-        }
-        assert!(image_alt == 2);
-        assert!(image_src == 2);
-        assert!(errors == 1);
-
-        Ok(())
-    }
-
-    #[test]
-    fn link() -> Result<(), io::Error> {
-        let t = lex(&fs::read_to_string("tests/link.md")?);
-        let mut link_text: usize = 0;
-        let mut link_href: usize = 0;
-        let mut errors: usize = 0;
-        for token in t.iter() {
-            match token.id {
-                TokenType::LinkText => {
-                    link_text += 1;
-                },
-                TokenType::LinkHref => {
-                    link_href += 1;
-                },
-                TokenType::Error => {
-                    errors += 1;
-                },
-                TokenType::Text|TokenType::Space|TokenType::Newline|TokenType::Whitespace(' ') => (),
-                _ => panic!("Encounterd TokenType other than expected!"),
-            }
-        }
-        assert!(link_text == 2);
-        assert!(link_href == 2);
-        assert!(errors == 1);
-
-        Ok(())
-    }
-
-    #[test]
-    fn horizontalrule() -> Result<(), io::Error> {
-        let t = lex(&fs::read_to_string("tests/horizontalrule.md")?);
-        let mut hr: usize = 0;
-        for token in t.iter() {
-            match token.id {
-                TokenType::HorizontalRule => {
-                    hr += 1;
-                },
-                TokenType::Text|TokenType::Space|TokenType::Newline|TokenType::Whitespace(' ') => (),
-                _ => panic!("Encounterd TokenType other than expected!"),
-            }
-        }
-        assert!(hr == 1);
-
-        Ok(())
-    }
-
-    #[test]
-    fn blockqoute() -> Result<(), io::Error> {
-        let t = lex(&fs::read_to_string("tests/blockquote.md")?);
-        let mut bb: usize = 0;
-        let mut be: usize = 0;
-        for token in t.iter() {
-            match token.id {
-                TokenType::BlockquoteBegin => {
-                    bb += 1;
-                },
-                TokenType::BlockquoteEnd => {
-                    be += 1;
-                },
-                TokenType::Text|TokenType::Space|TokenType::Newline|TokenType::Whitespace(' ') => (),
-                _ => panic!("Encounterd TokenType other than expected!"),
-            }
-        }
-        assert!(bb == 2);
-        assert!(be == 2);
-
-        Ok(())
-    }
-
-    #[test]
-    fn code() -> Result<(), io::Error> {
-        let t = lex(&fs::read_to_string("tests/code.md")?);
-        let mut code: usize = 0;
-        for token in t.iter() {
-            match token.id {
-                TokenType::Code => {
-                    code += 1;
-                },
-                TokenType::Text|TokenType::Space|TokenType::Newline|TokenType::Whitespace(' ') => (),
-                _ => panic!("Encounterd TokenType other than expected!"),
-            }
-        }
-        assert!(code == 2);
-
-        Ok(())
-    }
-
-    #[test]
-    fn codeblock() -> Result<(), io::Error> {
-        let t = lex(&fs::read_to_string("tests/codeblock.md")?);
-        let mut cbb: usize = 0;
-        let mut cbe: usize = 0;
-        let mut cbl: usize = 0;
-        for token in t.iter() {
-            match token.id {
-                TokenType::CodeBlockBegin => {
-                    cbb += 1;
-                },
-                TokenType::CodeBlockEnd => {
-                    cbe += 1;
-                },
-                TokenType::CodeBlockLanguage => {
-                    cbl += 1;
-                },
-                TokenType::Text|TokenType::Space|TokenType::Newline|TokenType::Whitespace(' ') => (),
-                _ => panic!("Encounterd TokenType other than expected!"),
-            }
-        }
-        assert!(cbb == 3);
-        assert!(cbe == 2);
-        assert!(cbl == 2);
-
-        Ok(())
-    }
-
-    #[test]
-    fn indentblock() -> Result<(), io::Error> {
-        let t = lex(&fs::read_to_string("tests/indentblock.md")?);
-        let mut indent: usize = 0;
-        for token in t.iter() {
-            match token.id {
-                TokenType::IndentBlock => {
-                    indent += 1;
-                },
-                TokenType::Text|TokenType::Space|TokenType::Newline|TokenType::Whitespace(' ') => (),
-                _ => panic!("Encounterd TokenType other than expected!"),
-            }
-        }
-        assert!(indent == 4);
-
-        Ok(())
-    }
-
-    #[test]
-    fn escape() -> Result<(), io::Error> {
-        let t = lex(&fs::read_to_string("tests/escape.md")?);
-        let mut esc: usize = 0;
-        for token in t.iter() {
-            match token.id {
-                TokenType::Escape => {
-                    esc += 1;
-                },
-                TokenType::Text|TokenType::Space|TokenType::Newline|TokenType::Whitespace(' ')|TokenType::Heading
-                |TokenType::ItalicBegin|TokenType::ItalicEnd|TokenType::BoldBegin|TokenType::BoldEnd
-                |TokenType::StrikeBegin|TokenType::StrikeEnd|TokenType::UnderlineBegin|TokenType::UnderlineEnd => (),
-                _ => panic!("Encounterd TokenType other than expected!"),
-            }
-        }
-        assert!(esc == 10);
-
-        Ok(())
-    }
-
-    #[test]
-    fn emphasis() -> Result<(), io::Error> {
-        let t = lex(&fs::read_to_string("tests/emphasis.md")?);
-        let mut i: usize = 0;
-        let mut b: usize = 0;
-        let mut s: usize = 0;
-        let mut u: usize = 0;
-        for token in t.iter() {
-            match token.id {
-                TokenType::ItalicBegin|TokenType::ItalicEnd => {
-                    i += 1;
-                },
-                TokenType::BoldBegin|TokenType::BoldEnd => {
-                    b += 1;
-                },
-                TokenType::StrikeBegin|TokenType::StrikeEnd => {
-                    s += 1;
-                },
-                TokenType::UnderlineBegin|TokenType::UnderlineEnd => {
-                    u += 1;
-                },
-                TokenType::Text|TokenType::Space|TokenType::Newline|TokenType::Whitespace(' ') => (),
-                _ => panic!("Encounterd TokenType other than expected!"),
-            }
-        }
-        assert!(i == 6);
-        assert!(b == 4);
-        assert!(s == 2);
-        assert!(u == 2);
-
-        Ok(())
-    }
-
-    #[test]
-    fn html() -> Result<(), io::Error> {
-        let t = lex(&fs::read_to_string("tests/html.md")?);
-        for token in t.iter() {
-            match token.id {
-                TokenType::Text|TokenType::Space|TokenType::Newline => (),
-                _ => panic!("Encounterd TokenType other than expected!"),
-            }
-        }
+    fn log_tokens(tokens: Vec<Token>, output: &str) -> Result<(), io::Error> {
+        let mut log = fs::File::create(format!("log/{}.log", output.to_string()))?;
+        log.write(format!("{:#?}", tokens).as_bytes())?;
 
         Ok(())
     }
--- tests/test.rs
+++ tests/test.rs
@@ -1,42 +1,6 @@
-use std::fs;
-use std::io;
-use std::io::Write;
-
-use markdowner;
-
-#[test]
-fn log() -> Result<(), io::Error> {
-    let test_files = [
-        "heading",
-        "checkbutton",
-        "image",
-        "link",
-        "horizontalrule",
-        "blockquote",
-        "code",
-        "codeblock",
-        "indentblock",
-        "escape",
-        "emphasis",
-        "html",
-    ];
-
-    for test in test_files.iter() {
-        let r = markdowner::markdown_to_html(format!("tests/{}.md", test).as_str(),
-                                format!("generated_html/{}.html", test).as_str());
-
-        match r {
-            Ok(tokens) => log_tokens(tokens, test)?,
-            Err(e) => return Err(e),
-        }
-    }
-
-    Ok(())
-}
-
-fn log_tokens(tokens: Vec<markdowner::Token>, output: &str) -> Result<(), io::Error> {
-    let mut log = fs::File::create(format!("log/{}.log", output.to_string()))?;
-    log.write(format!("{:#?}", tokens).as_bytes())?;
-
-    Ok(())
-}
+//use markdowner;
+// TODO Once the basics are complete this will test
+// all supported elements in one file using the public api.
+// There will be another test which might need an --ignore attribute
+// it will test the same things but repeated to check performance
+// on large inputs.
