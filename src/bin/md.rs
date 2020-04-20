ebfbd1e6d8b73f6251fd6c8319d76f3192ebb1d4
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2020-04-20 13:54:47 +0100

    bin: Added user message when there werent enough args

--- src/bin/md.rs
+++ src/bin/md.rs
@@ -5,10 +5,15 @@ use markdowner;
 
 fn main() -> Result<(), io::Error> {
     let args: Vec<String> = env::args().collect();
-    let input = args.get(1).unwrap().as_str();
-    let output = args.get(2).unwrap().as_str();
 
-    markdowner::markdown_to_html(input, output)?;
+    if args.len() < 3 {
+        println!("ERROR: The program needs two arguments!");
+        println!("\t$ md <input> <output>");
+    } else {
+        let input = args.get(1).unwrap().as_str();
+        let output = args.get(2).unwrap().as_str();
+        markdowner::markdown_to_html(input, output)?;
+    }
 
     Ok(())
 }
