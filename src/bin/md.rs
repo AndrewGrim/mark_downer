73c7b9d04d89611dcc553ed46a126edbf27d9daf
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2020-05-07 14:59:11 +0100

    bin: Changed args parsing...
    Check for the correct amount of arguments.
    Css arg is now optional.
    Added some comments to elucidate this fact.

--- src/bin/md.rs
+++ src/bin/md.rs
@@ -6,14 +6,21 @@ use markdowner;
 fn main() -> Result<(), io::Error> {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() < 4 {
-        println!("ERROR: The program needs three arguments!");
-        println!("\t$ md <input> <output> <css>");
-    } else {
+    if args.len() == 3 {
+        // Convert Markdown to HTML without css.
+        let input = args.get(1).unwrap().as_str();
+        let output = args.get(2).unwrap().as_str();
+        markdowner::markdown_to_html(input, output, "")?;
+    } else if args.len() == 4 {
+        // Convert Markdown to HTML and embed CSS.
         let input = args.get(1).unwrap().as_str();
         let output = args.get(2).unwrap().as_str();
         let css = args.get(3).unwrap().as_str();
         markdowner::markdown_to_html(input, output, css)?;
+    }  else {
+        println!("ERROR: Wrong number of arguments!");
+        println!("\t$ md <input> <output> <css>");
+        println!("NOTE: The <css> argument is optional.");
     }
 
     Ok(())
